607e19c537d23c99cb03a7a77b56645573986cc9
Christian Steinmetz <csteinmetz1@gmail.com>
2017-11-01 23:23:32 -0400

    main preprocess functions - validated as input to LDA model

--- analyze.py
+++ analyze.py
@@ -3,6 +3,10 @@ import scipy.fftpack
 import matplotlib.pyplot as plt
 import sunau
 
+import gensim
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
 def loadAU(filename):
     signal_fp = sunau.open(filename, 'r')
     sample_rate = signal_fp.getframerate()
@@ -58,15 +62,14 @@ def frameSignal(signal, sample_rate, frame_length=2048, frame_step=1024):
     return frames
 
 # generates bag of frequencies for one audio excerpt
-def generate_bof(frames, sample_rate, num_freq=2048, pow_fft=True):
+def generate_bof(frames, sample_rate, fft_size=2048, pow_fft=True):
     
-    mag_frames = np.absolute(np.fft.rfft(frames, num_freq))
-    pow_frames = ((1.0 / num_freq) * ((mag_frames) ** 2))
+    mag_frames = np.absolute(np.fft.rfft(frames, fft_size))
+    pow_frames = ((1.0 / fft_size) * ((mag_frames) ** 2))
     
-    freq = np.arange(0, sample_rate/2, sample_rate/len(mag_frames))
-
     if pow_fft:
         freqVec = np.sum(pow_frames, axis=0)/len(pow_frames)
+        freqVec = freqVec[0:len(freqVec)-1] # need to figure out why the length is too long
     else:
         freqVec = np.sum(mag_frames, axis=0)/len(mag_frames)
 
@@ -75,23 +78,50 @@ def generate_bof(frames, sample_rate, num_freq=2048, pow_fft=True):
 
     # stuff bag of frequencies 
     for power in np.nditer(freqVec):
-        bof.append((fid, float(power)))
+        bof.append((fid, int(power)))
         fid += 1
 
     return bof
 
 # generates "corpus" - list of bag of frequencies for audio excerpts
 def generate_corpus(bofs):
+    pass
 
+def generate_dictionary(sample_rate, fft_size):
+    
+    dictionary = {}
+    fid = 0  # frequency id
+    
+    freqs = np.arange(0, float(sample_rate)/2.0, float(sample_rate)/float((fft_size)), dtype=float)
+
+    for freq in freqs:
+        dictionary[fid] = str(int(np.ceil(freq)))
+        fid += 1 
+
+    print dictionary
+    return dictionary
+
+def train_model(dictionary, corpus, num_topics):
+
+    Lda = gensim.models.ldamodel.LdaModel
+    ldamodel = Lda(corpus, num_topics=num_topics, id2word = dictionary, passes=16)
+
+    return ldamodel
 
 rock01 = "genres/rock/rock.00005.au" # 30 sec clip from dataset
 data, Fs = loadAU(rock01)
 
 frames = frameSignal(data, Fs)
 bof = generate_bof(frames, Fs)
+dictionary = generate_dictionary(Fs, 2048)
+
+corpus = []
+corpus.append(bof)
+
+train_model(dictionary, corpus, 50)
 
 
-plt.plot(bof[:][0], bof[:][1])
-plt.yscale('log')
-plt.xscale('log')
-plt.show()
+#plt.plot(bof[:][0], bof[:][1])
+#plt.yscale('log')
+#plt.xscale('log')
+#plt.show()
