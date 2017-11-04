8939c40c8ed7cb54eae069a953709bd1bde97296
Christian Steinmetz <csteinmetz1@gmail.com>
2017-11-04 11:33:05 -0400

    updated training to use only 80% of dataset

--- test.py
+++ test.py
@@ -9,12 +9,25 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 import analysis # provides specilized methods for loading files and preprocessing
 
+# first load a file not used in training
+data, fs = analysis.loadau("genres/rock/rock.00080.au")
 
-# first load the file
-data, fs = analysis.loadau("genres/rock/rock.00000.au")
+# generate seed bag of frequencies vector
+bof_seed = analysis.generateBagOfFrequencies(data, fs)
 
-# generate the bag of frequencies vector for the file
-bof = analysis.generateBagOfFrequencies(data, fs)
+library = [] # to store list of all bof vectors for music library
+rootdir = "genres/" # directory of dataset
+
+# load all of the potential recommendations
+for genre in os.listdir(rootdir):
+    if not genre.endswith(".DS_Store"): # ignore these annoying MacOS X files
+        print genre
+        for filename in os.listdir(rootdir + genre):
+            if filename.endswith(".au"): 
+                print int(filename[filename.find(".")+1:filename.rfind(".")]) 
+                data, fs = analysis.loadau(rootdir + genre + "/" + filename)
+                bof = analysis.generateBagOfFrequencies(data, fs)
+                library.append(bof)
 
 # load the model from training
 ldamodel = gensim.models.LdaModel.load("model/lda.model")
